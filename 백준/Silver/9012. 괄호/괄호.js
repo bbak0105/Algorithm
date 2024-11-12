0eadbc1a3ec41ca184b83c3b08f9e6131b9c8d89
Dev Kero <66405572+bbak0105@users.noreply.github.com>
2024-11-12 17:33:04 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 9516 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,3 +1,4 @@
+
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n').filter(line => line.trim() !== '');  
 
@@ -5,9 +6,8 @@ const [num, ...array] = input
 
 function findVPS(array) {
     for(let i=0; i<array.length; i++) {
-        const word = array[i].trim()
+        const word = array[i]
         let stack = 0
-        let VPS = true
 
         for(let j=0; j<word.length; j++) {
             const text = word[j]
@@ -18,19 +18,12 @@ function findVPS(array) {
 
             if(text === ')') {
                 stack--;
-
-                if(stack < 0) {
-                    VPS = false
-                    break;
-                }
+                
+                if(stack < 0) break;
             }
         }
 
-        if(stack !== 0) {
-            VPS = false
-        }
-
-        console.log(VPS ? 'YES' : 'NO')
+        console.log(stack === 0 ?  'YES' : 'NO')
     }
 }
 
